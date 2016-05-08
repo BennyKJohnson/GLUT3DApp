0c6351da23715bdc9cd33c11c3b430e57fc3d59e
BennyKJohnson <bkj986@uowmail.edu.au>
2016-05-08 12:14:28 +1000

    Initial Commit

---
+++ GLUT3DApp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GLUT3DApp
+//
+//  Created by Ben Johnson on 8/05/2016.
+//  Copyright © 2016 Benjamin Johnson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
